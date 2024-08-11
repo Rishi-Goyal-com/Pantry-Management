db683ea48e88fa3a5850099cfb4a5db6d6d30a36
Rishi-Goyal-com <goyalrishi300@gmail.com>
2024-08-11 02:39:56 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth,GoogleAuthProvider  } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyA9ItBCjV8BDS1SLgDNhFfSXfaVokfjz7U",
-  authDomain: "inventory-management-cbc87.firebaseapp.com",
-  projectId: "inventory-management-cbc87",
-  storageBucket: "inventory-management-cbc87.appspot.com",
-  messagingSenderId: "337508076549",
-  appId: "1:337508076549:web:3b374102e634244219606c",
-  measurementId: "G-1C2BJ34X4P"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
